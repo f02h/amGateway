31101ef1d09df7e31eb158db0d5ca9696afc2239
David Starič <david.staric@klaro.si>
2018-11-27 14:16:05 +0100

    .

--- app/Http/Controllers/ApiMsgController.php
+++ app/Http/Controllers/ApiMsgController.php
@@ -35,29 +35,15 @@ class ApiMsgController extends Controller
         return response()->json(['status' => 'success','result' => $todo]);
     }
 
-    public function getMessages($action, Request $request)
+    public function getMessages( Request $request)
     {
         $idGateway = $request->input('gateway');
         if (!$idGateway) {
             return response()->json(['status' => 'error', 'msg' => 'No gateway parameter set.']);
         }
 
-        switch ($action) {
-            case 'transfer_in':
-                $action = EPP::DOMAIN_TRANSFER_IN;
-                break;
-            case 'transfer_out':
-                $action = EPP::DOMAIN_TRANSFER_OUT;
-                break;
-            case 'all':
-                $action = '';
-                break;
-            default:
-                break;
-        }
-
         $result = array();
-        foreach (Msg::select()->where('idGateway','like', '%'.$idGateway.'%')->where('msgAction','like', '%'.$action.'%')->get() as $msg) {
+        foreach (Msg::select()->where('idGateway','like', '%'.$idGateway.'%')->get() as $msg) {
             $result[] = json_decode($msg->msg);
         }
 
