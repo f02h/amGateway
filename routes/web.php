d0bbd11c92ddcece15a4e67cabbdce6c94d3c6f6
David Starič <david.staric@klaro.si>
2018-12-13 10:35:41 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ $router->get('/msg/', function () use ($router) {
 });
 
 //$router->get('msg/{id}/', 'MsgController@show');
-$router->get('/msgs/{action}', 'ApiMsgController@getMessages');
+$router->get('/msgs/read/{action}', 'ApiMsgController@getMessages');
 $router->get('/msgs/confirm/', 'ApiMsgController@confirmMessages');
 $router->get('/msgs/release/', 'ApiMsgController@releaseMessages');
 
